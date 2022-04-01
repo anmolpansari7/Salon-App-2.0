eff2a25e34074df6c17e2a792dd0811dbdb60098
Anmol Pansari <anmolpansari7@gmail.com>
2022-04-01 16:45:33 +0530

    displaying active pack in customer info pack

--- src/components/customers/CurrentCustomerActivePackage.js
+++ src/components/customers/CurrentCustomerActivePackage.js
@@ -4,6 +4,14 @@ import CardHeading from "./../custom_ui/CardHeading";
 import ListItemDBtn from "../custom_ui/ListItemDBtn";
 import { useSelector } from "react-redux";
 import moment from "moment";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
 
 const CurrentCustomerActivePackage = () => {
   const currCustomer = useSelector(
@@ -17,41 +25,62 @@ const CurrentCustomerActivePackage = () => {
       <CardHeading className=" font-medium text-base flex-1">
         Active Packages
       </CardHeading>
-      <div className=" flex flex-col space-y-3 mt-5">
+      {/* <div className=" flex flex-col space-y-3 mt-5">
         <ListItemDBtn
           content={"Pack Name"}
           content2={"Great Khali"}
           showBtn={false}
           className=" text-sm"
         />
-        <ListItemDBtn
-          content={"Created On"}
-          content2={"Feb 12 2010"}
-          showBtn={false}
-          className=" text-sm"
-        />
-        <ListItemDBtn
-          content={"Valid till"}
-          content2={"Mar 12 2010"}
-          showBtn={false}
-          className=" text-sm"
-        />
-        <ListItemDBtn
-          content={"Pack Amount"}
-          content2={"520 Rs."}
-          showBtn={false}
-          className=" text-sm"
-        />
-        <ListItemDBtn
-          content={"Actual Amount"}
-          content2={"600 Rs."}
-          showBtn={false}
-          className=" text-sm"
-        />
       </div>
       <div className=" border border-gray-400 rounded-md h-24 mt-3 px-2 py-1">
         <span className=" text-gray-400 text-sm">Display Items</span>
-      </div>
+      </div> */}
+      <Accordion allowMultiple>
+        {currCustomer.package !== undefined &&
+          currCustomer.package.map((pack) => {
+            if (pack.packageName !== undefined) {
+              return (
+                <AccordionItem size={"sm"}>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        {pack.packageName}
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4} className="flex flex-col space-y-2">
+                    <ListItemDBtn
+                      content={"Usage Left"}
+                      content2={pack.UsageLeft}
+                      showBtn={false}
+                      className=" text-sm"
+                    />
+                    <ListItemDBtn
+                      content={"Valid till"}
+                      content2={moment(pack.validTill).format("ll")}
+                      showBtn={false}
+                      className=" text-sm"
+                    />
+                    <ListItemDBtn
+                      content={"Pack Amount"}
+                      content2={pack.packageAmount}
+                      showBtn={false}
+                      className=" text-sm"
+                    />
+                    <ListItemDBtn
+                      content={"Actual Amount"}
+                      content2={pack.totalAmount}
+                      showBtn={false}
+                      className=" text-sm"
+                    />
+                  </AccordionPanel>
+                </AccordionItem>
+              );
+            }
+          })}
+      </Accordion>
     </Card>
   );
 };
